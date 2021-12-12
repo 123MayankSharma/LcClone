68aa219957308d2eb630bf67d6da30ceffa99134
123MayankSharma <Mayanksharma922001@gmail.com>
2021-12-13 00:48:29 +0530

    now it supports multiple test cases :)

--- server/code.py
+++ server/code.py
@@ -1,7 +1,7 @@
 import sys
 
 def add(a,b):
-  return a+a
+  return a+b
 
 if __name__ == "__main__":
   a=int(sys.argv[1])
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import { writeFileSync } from 'fs'
 import express, { json } from 'express'
 import cors from 'cors'
 import { PythonShell } from 'python-shell'
-let a = false
+let a = []
 
 const app = express()
 
@@ -11,28 +11,54 @@ const port = 3001
 app.use(cors({ origin: true, credentials: true }))
 app.use(json())
 
+//these are the test cases on which code will be run!
+const testCases = [
+    [1, 2, 3],
+    [2, 2, 4],
+    [5, 4, 8]
+]
+
 // Configuration of python shell to run code file
-let options = {
-    mode: "text",
-    pythonOptions: ['-u'], // it unbuffers the streams(stdin,stdout)
-    args: [1, 2, 3]
-}
+// let options = {
+//     mode: "text",
+//     pythonOptions: ['-u'], // it unbuffers the streams(stdin,stdout)
+//     args: []
+// }
 
 
 app.post("/py", (req, res) => {
     writeFileSync("code.py", req.body.code);
+    for (let index = 0; index < testCases.length; index++) {
+        PythonShell.run("code.py", {
+            mode: "text",
+            pythonOptions: ['-u'], // it unbuffers the streams(stdin,stdout)
+            args: testCases[index]
+        }, (err, result) => {
+            if (err) throw err;
 
-    PythonShell.run("code.py", options, (err, result) => {
-        if (err) throw err;
+            // console.log(`result: ${result}`)
+            a.push(result[0]);
+        })
 
-        console.log(`result: ${result}`)
-        a = result
-    })
+
+    }
+    // PythonShell.run("code.py",  {
+    // mode: "text",
+    // pythonOptions: ['-u'], // it unbuffers the streams(stdin,stdout)
+    // args: testCases[i];
+    // } , (err, result) => {
+    //     if (err) throw err;
+
+    //     console.log(`result: ${result}`)
+    //     a.push_back(result[0]);
+    // })
+    console.log(a);
 
 
     res.json({
-        Message:a[0]
+        message: a
     })
+    a = []
 })
 
 
